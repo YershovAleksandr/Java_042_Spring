7de8d7aded62baa37dcdaade7b7a4b6126a585b8
hp <namstudionsk@gmail.com>
2019-01-16 11:38:11 +0700

    Wip

--- src/main/java/com/nam/spring_42/Controller2.java
+++ src/main/java/com/nam/spring_42/Controller2.java
@@ -7,12 +7,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller2 {
+
     @GetMapping("/x")
     public String index(){
         return "<h3>Hello</h3><br /><h2>World</h2>";
     }
+
     @RequestMapping("/x2")
     public String index2(@RequestParam(value="id", defaultValue = "42") String id){
         return String.format("<b>Id = </b><i>%s</i>", id);
+    }
+
+    @RequestMapping("/x3")
+    public String index3(@RequestParam(value="id", defaultValue = "11") String id, @RequestParam(value = "gid", defaultValue = "22") String gid){
+        return String.format("<h2>ID = %s</h2><h4>GID = %s</h4>", id, gid);
     }
 }
