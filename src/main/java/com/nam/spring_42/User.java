9b76f58f5c303a478731f905c912b83829995e6d
hp <namstudionsk@gmail.com>
2019-01-19 23:22:10 +0700

    Wip

---
+++ src/main/java/com/nam/spring_42/User.java
@@ -0,0 +1,35 @@
+package com.nam.spring_42;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+//@XmlRootElement
+public class User {
+    private int id;
+    private String name;
+
+    public User(){
+        id = 42;
+        name = "X42";
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("Id = %d, Name = %s", id, name);
+    }
+}
