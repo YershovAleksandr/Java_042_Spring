b5af2d4ed215edc6ea48d2b896e2551e5d549441
hp <namstudionsk@gmail.com>
2019-01-25 23:57:53 +0700

    Wip

--- src/main/java/com/nam/spring_42/board/Main.java
+++ src/main/java/com/nam/spring_42/board/Main.java
@@ -2,11 +2,12 @@ package com.nam.spring_42.board;
 
 import com.nam.spring_42.board.dao.PostDao;
 import com.nam.spring_42.board.model.Post;
+import com.nam.spring_42.board.services.PostService;
 
 public class Main {
     public static void main(String[] args){
 
-        PostDao postDao = new PostDao();
+        /*PostDao postDao = new PostDao();
 
         Post post1 = new Post();
         post1.setId(0);
@@ -20,10 +21,30 @@ public class Main {
         post2.setTitle("Title2");
         post2.setText("Text2");
 
-        postDao.create(post1);
+        postDao.create(post2);
 
 
-        postDao.getAll().forEach(post -> System.out.println(post));
+        postDao.getAll().forEach(post -> System.out.println(post));*/
+
+        PostService postService = new PostService();
+
+        Post post1 = new Post();
+        post1.setId(0);
+        post1.setTitle("Title1");
+        post1.setText("Text1");
+
+        postService.postCreate(post1);
+
+        Post post2 = new Post();
+        post2.setId(1);
+        post2.setTitle("Title2");
+        post2.setText("Text2");
+
+        postService.postCreate(post2);
+
+
+        postService.getAllPosts().forEach(post -> System.out.println(post));
+
 
     }
 }
