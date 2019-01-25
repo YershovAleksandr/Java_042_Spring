c5e4d152d7d9112567881ab3c17a5e5b0a86bc0c
hp <namstudionsk@gmail.com>
2019-01-25 23:54:50 +0700

    Wip

--- src/main/java/com/nam/spring_42/board/services/PostService.java
+++ src/main/java/com/nam/spring_42/board/services/PostService.java
@@ -1,4 +1,30 @@
 package com.nam.spring_42.board.services;
 
-public class PostService implements IService {
+import com.nam.spring_42.board.dao.PostDao;
+import com.nam.spring_42.board.model.Post;
+
+import java.util.List;
+
+public class PostService {
+    private PostDao postDao = new PostDao();
+
+    public List<Post> getAllPosts(){
+        return postDao.getAll();
+    }
+
+    public Post getPostById(int id){
+        return postDao.get(id);
+    }
+
+    public void postUpdate(Post post){
+        postDao.update(post);
+    }
+
+    public void postCreate(Post post){
+        postDao.create(post);
+    }
+
+    public void postDelete(Post post){
+        postDao.delete(post);
+    }
 }
