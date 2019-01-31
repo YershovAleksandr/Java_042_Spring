7cc63dd7ae763501c4cf34e469977619a5c209db
hp <namstudionsk@gmail.com>
2019-01-31 20:53:46 +0700

    Wip

--- src/main/java/com/nam/spring_42/board/utils/Tools.java
+++ src/main/java/com/nam/spring_42/board/utils/Tools.java
@@ -1,4 +1,8 @@
 package com.nam.spring_42.board.utils;
 
 public class Tools {
+    public static String url;
+    public static String username;
+    public static String password;
+    public static String driverClassName;
 }
